55db9e0f39e0c5db53d7f40c3a0a10f8b848751e
Nick Rasp <nickrasp03@gmail.com>
2022-09-28 17:28:36 -0400

    Starting to add JS element.

---
+++ app.js
@@ -0,0 +1,8 @@
+const splashText = document.querySelector('#splash-text')
+console.log(splashText)
+const splashImage = document.querySelector('#splash-img')
+console.log(splashImage)
+
+splashImage.addEventListener('click', () => {
+    console.log('Hello World')
+})
